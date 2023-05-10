d788049b97ceb9988e5c09fe731f9715422ac501
Shayan <skhos068@uottawa.ca>
2023-05-10 00:45:25 -0400

    updated

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # todo
-# - put tracker in seperate thread to minimize fps loss
 # - implement auto flight control (homing)
 # - implment manual flight control
 
 from djitellopy import Tello
 import cv2, math, time, threading
 import numpy as np
+
+prev_frame_time = 0
+new_frame_time = 0
 
 init_alt = 0
 relative_alt = 0
@@ -22,16 +24,20 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 line_type = 1
 manual_control = True
+empty_frame = None
 
+lock = threading.Lock()
+thread_init = False
+reset_track = False
+tracker_thread = None
 tracker = None
 tracking = False
+draw_bbox = None
 bbox = None
-first_pointB = False
-second_pointB = False
+tracker_ret = False
 first_point = (0, 0)
 second_point = (0, 0)
 point_counter = 0
-
 
 tello = Tello()
 tello.connect()
@@ -43,13 +49,12 @@ try:
 except:
     print("[FEED] - No Feed Signal")
 
-
-# Mouse callback function
 def onMouse(event, x, y, flags, param):
-    global tracker, tracking, bbox, point_counter, first_point, second_point
+    global tracker, tracking, bbox, point_counter, first_point, second_point, reset_track
+
+    print("MOUSE CALLED")
 
     if event == cv2.EVENT_LBUTTONDOWN:
-        
         if point_counter == 0:
             print("first point set to ", x, y)
             first_point = (x, y)
@@ -63,24 +68,39 @@ def onMouse(event, x, y, flags, param):
         if tracking and point_counter == 3:
             point_counter = 0
             tracking = False
-            print("reset")
+            reset_track = True
+            tracker = None
+            print("ROI RESET")
 
         if point_counter == 2:
+            reset_track = False
             bbox = (first_point[0], first_point[1], second_point[0], second_point[1])
             tracker = cv2.legacy.TrackerCSRT_create()
             tracker.init(empty_frame, bbox)
             tracking = True
-            
+
+def run_tracker():
+    global tracking, empty_frame, tracker, draw_bbox, tracker_ret, thread_init, reset_track
+    lock.acquire()
+    thread_init = True
+    while tracking:
+        tracker_ret, draw_bbox = tracker.update(empty_frame)
+        if tracker_ret == False or reset_track:
+            tracking = False
+
+    thread_init = False
+    tracker_thread = None
+    lock.release()
+    print("TRACKING THREAD TERMINATED")
 
 cv2.namedWindow("FEED", cv2.WINDOW_NORMAL)
 cv2.setMouseCallback("FEED", onMouse)
-
 
 while True:
     try:
         frame = frame_read.frame
         empty_frame = frame.copy()
-        empty_frame = cv2.cvtColor(empty_frame, cv2.COLOR_BGR2RGB)
+        #empty_frame = cv2.cvtColor(empty_frame, cv2.COLOR_BGR2RGB)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         relative_alt = (tello.get_barometer() - init_alt) * 0.0328
@@ -107,51 +127,58 @@ while True:
         cv2.putText(frame, "YPR  {}  {}  {}".format(tello.get_pitch(), tello.get_roll(), tello.get_height()), (5, height - 10), font, font_scale, white, line_type)
 
         # top right
+        new_frame_time = time.time()
+        fps = 1/(new_frame_time-prev_frame_time)
+        prev_frame_time = new_frame_time
+        fps_size = cv2.getTextSize("FPS  {}".format(str(int(fps))), font, font_scale, line_type)[0][0]
+        cv2.putText(frame, "FPS  {}".format(str(int(fps))), (width - fps_size - 5, 25), font, font_scale, white, line_type)
+
         time_size = cv2.getTextSize("T + {}".format(tello.get_flight_time()), font, font_scale, line_type)[0][0]
-        cv2.putText(frame, "T + {}".format(tello.get_flight_time()), (width - time_size, 25), font, font_scale, white, line_type)
+        cv2.putText(frame, "T + {}".format(tello.get_flight_time()), (width - time_size - 5, 55), font, font_scale, white, line_type)
 
         # top center
         if (manual_control):
-            
             cv2.rectangle(frame, (width//2 - 20, 10), (width//2 + 30, 28), white, -1)
             cv2.putText(frame, "MANL", (width//2 - 20, 25), font, font_scale, black, line_type)
-
         else:
-
             cv2.rectangle(frame, (width//2 - 20, 10), (width//2 + 30, 28), white, -1)
             cv2.putText(frame, "AUTO", (width//2 - 20, 25), font, font_scale, black, line_type)
 
         if tracking:
             lock_size = cv2.getTextSize("LOCK", font, font_scale, line_type)[0][0]
-            cv2.rectangle(frame, (width//2 - (lock_size // 2), height - 38), (width//2 + lock_size - 25, height - 20), white, -1)
-            cv2.putText(frame, "LOCK", (width//2 - (lock_size // 2), height - 23), font, font_scale, black, line_type)
+            cv2.rectangle(frame, (width // 2 - (lock_size // 2), height - 38), (width // 2 + lock_size - 25, height - 20), white, -1)
+            cv2.putText(frame, "LOCK", (width // 2 - (lock_size // 2), height - 23), font, font_scale, black, line_type)
 
+        if tracking and thread_init == False:
+            print("TRACKING THREAD STARTED")
+            tracker_thread = threading.Thread(target=run_tracker)
+            tracker_thread.start()
 
-        if tracking:
-            ret, bbox = tracker.update(empty_frame)
+        if tracking == False and thread_init == False and tracker_thread:
+            print("THREAD RESET")
+            tracker_thread.join()
+            tracker_thread = None
 
-            if ret:
-                # Draw the tracked object on the frame
-                x, y, w, h = [int(value) for value in bbox]
+        if tracker_ret and tracking:
+            x, y, w, h = [int(value) for value in draw_bbox]
                 # top left
-                cv2.line(frame, (x, y), (x + 50, y), white, 1)
-                cv2.line(frame, (x, y), (x, y + 50), white, 1)
+            cv2.line(frame, (x, y), (x + 50, y), white, 1)
+            cv2.line(frame, (x, y), (x, y + 50), white, 1)
                 # top right
-                cv2.line(frame, (x + w, y), (x + w - 50, y), white, 1)
-                cv2.line(frame, (x + w, y), (x + w, y + 50), white, 1)
+            cv2.line(frame, (x + w, y), (x + w - 50, y), white, 1)
+            cv2.line(frame, (x + w, y), (x + w, y + 50), white, 1)
                 # bottom left
-                cv2.line(frame, (x, y + h), (x + 50, y + h), white, 1)
-                cv2.line(frame, (x, y + h), (x, y + h - 50), white, 1)
+            cv2.line(frame, (x, y + h), (x + 50, y + h), white, 1)
+            cv2.line(frame, (x, y + h), (x, y + h - 50), white, 1)
                 # bottom right
-                cv2.line(frame, (x + w, y + h), (x + w - 50, y + h), white, 1)
-                cv2.line(frame, (x + w, y + h), (x + w, y + h - 50), white, 1)
+            cv2.line(frame, (x + w, y + h), (x + w - 50, y + h), white, 1)
+            cv2.line(frame, (x + w, y + h), (x + w, y + h - 50), white, 1)
 
-            else:
 
-                tracking = False
-
-    except:
-        print("[FEED] - UI Error")
+    except Exception as error:
+        print("[FEED] - UI ERROR -", error)
+        if tracker_thread:
+            tracker_thread.join()
 
     try:
         cv2.imshow("FEED", frame)
@@ -159,7 +186,9 @@ while True:
         tello.streamoff()
         tello.end()
 
-    if (cv2.waitKey(1) & 0xff) == 27:
+    if (cv2.waitKey(1) & 0xff) == 27:        
+        if tracker_thread:
+            tracker_thread.join()
         break
 
 cv2.destroyAllWindows()
