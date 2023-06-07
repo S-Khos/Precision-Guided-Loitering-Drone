330801f4c1a65592b0d4d396f9316eb4a7f7f573
Shayan <skhos068@uottawa.ca>
2023-06-07 03:34:03 -0400

    updated drone_feed

--- drone_feed.py
+++ drone_feed.py
@@ -4,11 +4,12 @@ import threading
 from djitellopy import Tello
 import queue
 
-feed_queue = queue.Queue()
+feed_queue = queue.Queue(maxsize=1)
 drone = Tello()
 drone.connect()
 drone.streamon()
 frame_read = drone.get_frame_read()
+lock = threading.Lock()
 WIDTH = 960
 HEIGHT = 720
 
@@ -17,7 +18,10 @@ def receive():
     global frame_read, feed_queue
     while True:
         frame = frame_read.frame
-        feed_queue.put(frame)
+        if feed_queue.empty():
+            feed_queue.put(frame)
+
+        time.sleep(0.01)
 
 
 cv2.namedWindow("FEED", cv2.WINDOW_NORMAL)
@@ -30,14 +34,27 @@ def display():
     while True:
         if not feed_queue.empty():
             frame = feed_queue.get()
+            cv2.circle(frame, (int(WIDTH / 2), int(HEIGHT / 2)),
+                       100, (0, 0, 255), 40)
+            if feed_queue.empty():
+                feed_queue.put(frame)
+        time.sleep(0.012)
+
+
+def main():
+    global feed_queue
+    while True:
+        if not feed_queue.empty():
+            frame = feed_queue.get()
             cv2.imshow("FEED", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        time.sleep(0.013)
 
 
 thread1 = threading.Thread(target=receive, daemon=True)
-thread2 = threading.Thread(target=display)
+thread2 = threading.Thread(target=display, daemon=True)
 thread1.start()
 thread2.start()
-thread2join()
+main()
